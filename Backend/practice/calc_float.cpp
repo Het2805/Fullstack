5a556eef5e1d8e2b8eb7257414d9853a7f375017
Het2805 <hetpaghdal658@gmail.com>
2026-02-19 15:12:35 +0530

    Fullstack

---
+++ Backend/practice/calc_float.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+float a,b;
+
+printf("/nEnter your A :");
+scanf("%f",&a);
+printf("A is :");
+printf("/nEnter your b :");
+scanf("%f",&b);
+printf("B is :");
+printf("/na/b is :%.2f",a/b);
+return 0;
+}
---
+++ Backend/practice/marks_cal.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	float physics, maths, chemistry, total, avg;
+	
+	printf("\nEnter Marks obtained in physics: ");
+	scanf("%f",&physics);
+	printf("\nEnter Marks obtained in maths: ");
+	scanf("%f",&maths);
+	printf("\nMarks obtained in chemistry: ");
+	scanf("%f",&chemistry);
+	printf("\nMarks obtained in physics: %.2f",physics);
+	printf("\nMarks obtained in maths: %.2f",maths);
+	printf("\nMarks obtained in chemistry: %.2f",chemistry);
+	total = physics+chemistry+maths;
+	printf("\ntotal marks obtained : %.2f",total);
+	avg = total/300*100;
+	printf("\n average marks obtained : %.2f",avg);
+	
+	if(avg>100 || avg<0){
+	printf("\n Marks are invalid: %.2f",avg);
+	}
+	else if(avg>=80 && avg<=100){
+		printf("\nA grade %.2f",avg);
+	}
+	else if(avg>70 && avg<80){
+		printf("\nB grade %.2f",avg);
+	}
+	else if(avg>50 && avg<70){
+		printf("\nC grade %.2f",avg);
+	}
+	else if(avg>33 && avg<50){
+		printf("\nD grade %.2f",avg);
+	}
+	else{
+		printf("\nfell");
+	}
+	return 0;
+}
+
